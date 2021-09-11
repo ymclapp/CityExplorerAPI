04f3fe999b06d8c1359cf8f1759320958faa5bbe
ymcla <ymclapp@yahoo.com>
2021-09-10 20:33:08 -0500

    trying to get weather to load

--- server.js
+++ server.js
@@ -19,8 +19,10 @@ app.get('/', (request, response) => {
 });
 
 // const weather = require('./data/weather.json');
-app.get('/weather', getWeather);
-
+app.get('/weather', (request, response) => {
+  console.log('query', request.query);
+  response.send(getWeather());
+});
 
 //Make sure the server is listening for requests.  Adding http et al. now you can click on the link to see the localhost running
 app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}`));
@@ -28,18 +30,22 @@ app.listen(PORT, () => console.log(`App is listening on http://localhost:${PORT}
 //route handlers down here
 const axios = require('axios');
 
+
 async function getWeather(request, response) {
   const q = request.query.q;
-  let parts = 'alerts, minutely, hourly';
+  // let parts = 'alerts, minutely,current,hourly';
+  const lon = request.query.lon;
+  const lat = request.query.lat;
 
   try {
-    const results = await axios.get('https://api.openweathermap.org/data/2.5/onecall?', {
+    const results = await axios.get('https://api.openweathermap.org/data/2.5/forecast/daily', {
       params:  {
+        q,
+        lat: lat,
+        lon: lon,
+        cnt: 7,
         appid:  process.env.API_WEATHER_KEY,
-        latitude: lat,
-        longitude: lon,
-        query: q,
-        exclude: parts,
+        // query: q,
       },
     });
 
@@ -54,10 +60,10 @@ async function getWeather(request, response) {
   }
 }
 
+
 class Weather {
   constructor(weatherData) {
-    this.latitude = weatherData.lat;
-    this.description = weatherData.daily.weather.description;
-    this.longitude = weatherData.lon;
+    this.description = weatherData.forecast;
+    // this.time = new Date (weatherData.forecast.time.day).toDateString;
   }
 }
